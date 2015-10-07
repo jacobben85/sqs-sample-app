144f94faa25b659c449194396a58cd6d19040662
jacobben85 <jacobben85@gmail.com>
2015-10-07 13:28:46 -0400

    Added queue as properties

--- src/test/java/com/univision/deportes/services/sqs/QueueServiceTest.java
+++ src/test/java/com/univision/deportes/services/sqs/QueueServiceTest.java
@@ -3,13 +3,15 @@ package com.univision.deportes.services.sqs;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 /**
  * Created by jbjohn on 10/7/15.
  */
 public class QueueServiceTest {
 
     @Test
-    public void testProcess() {
+    public void testProcess() throws IOException {
         QueueService queueService = new QueueService();
         queueService.process();
     }
